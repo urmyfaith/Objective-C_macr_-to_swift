5d978ef15bed021275d375fb934dc5a0f64cf678
zx <urmyfaith@qq.com>
2016-03-04 23:38:46 +0800

    init

---
+++ testMacro/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  testMacro
+//
+//  Created by zx on 3/4/16.
+//  Copyright © 2016 zx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ testMacro/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  testMacro
+//
+//  Created by zx on 3/4/16.
+//  Copyright © 2016 zx. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
